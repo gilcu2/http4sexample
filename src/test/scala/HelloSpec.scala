06e7362b85f32a8d268f149394163308efb7d33e
Reynaldo <r.garcia@douglas.de>
2017-10-06 16:00:24 +0200

    added inc service

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -4,10 +4,11 @@ import org.http4s._
 import org.http4s.dsl._
 import org.scalatest._
 import com.gilcu2.http4sexample._
+import org.http4s.util.CaseInsensitiveString
 
 class HelloSpec extends FlatSpec with Matchers {
 
-  "A Hello service" should "return hello" in {
+  "Hello service" should "return hello name" in {
 
     val request = Request(Method.GET, uri("/hello/juan"))
     val task = HelloWorld.service.run(request)
@@ -15,6 +16,19 @@ class HelloSpec extends FlatSpec with Matchers {
     val response=maybeResponse.toOption.get
 
     response.status should be( Status.Ok)
+    response.headers.get(CaseInsensitiveString("Content-Type")).get.value should be("application/json")
+
+  }
+
+  "inc service" should "return number+1" in {
+    val number = 3
+    val request = Request(Method.GET, Uri.unsafeFromString(s"/hello/$number"))
+    val task = HelloWorld.service.run(request)
+    val maybeResponse = task.unsafeRun
+    val response = maybeResponse.toOption.get
+
+    response.status should be(Status.Ok)
+    response.body.toString.toInt should be(number + 1)
 
   }
 
